650412663c70d0fee1c3b0b5eb3b4f2a311d7f7a
Gerald Bryant <hakeem1106@gmail.com>
2018-09-21 23:15:21 -0400

    ???

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,14 +1,4 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-class Hello extends React.Component {
-    render() {
-        return React.createElement('div', null, `Hello ${this.props.toWhat}`);
-    }
-}
-
-ReactDOM.render(
-    React.createElement(Hello, { toWhat: 'World' }, null),
-    document.getElementById('root')
-);
+var name = document.createElement("div");
